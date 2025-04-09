cef6fdf0e06721b3bf91fd58ec188cf0fa537e42
Khant Thit Oo <127503879+travthegoat@users.noreply.github.com>
2025-04-09 22:18:20 +0630

    Filter Users With Username

--- server/core/urls.py
+++ server/core/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from user.views import UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include(router.urls)),
     path('api/auth/', include('djoser.urls.jwt')),
 ]
 
--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from djoser.views import UserViewSet as BaseUserViewSet
+from .models import User
 
 # Create your views here.
+class UserViewSet(BaseUserViewSet):
+    def get_queryset(self):
+        username = self.request.query_params.get('username')
+        if username:
+            return User.objects.filter(username=username)
+        return super().get_queryset()
+    
